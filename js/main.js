e69f8167117a71b6e65e21072cd1b4de286af344
Iisha Medina <imeddes@yahoo.com>
2016-03-19 00:10:40 -0700

    Zodiac project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Daily Horoscope.</button>A day when intense talking, may finally 
+win someone over. You are not known for 
+backing down, but more for your subtle 
+strategies of getting your own way. 
+Your penetrating insight into other 
+people's motives and personalities 
+helps you to drive home the kind of 
+deal you want. If you are wishing to 
+make headway in a certain relationship 
+then you may need to be even subtler in 
+order to really make an impression.
